4eb0e8a1ad44529e85ee0ae77b7c83ea81560899
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-07-03 19:36:17 +0900

    Update default.js

--- default.js
+++ default.js
@@ -63,8 +63,7 @@ var exdate = cltime.toUTCString();
 }else{
 s += "; ";
 }
-
-document.cookie = s;
+  document.cookie = s;
 }
   
   function getCookie(c_name){
@@ -73,9 +72,9 @@ document.cookie = s;
     if(0 < document.cookie.length){
     st=document.cookie.indexOf(c_name + "=");
       if(st!=-1){
-      st=st+c_name.legth+1;
+      st=st+c_name.length+1;
       ed=document.cookie.indexOf(";",st)
-       if(ed==-1) ed=document.cookie.legth;
+       if(ed==-1) ed=document.cookie.length;
         return unescape(document.cookie.substring(st,ed));
       }
     }
